dba1840eb089366c6537f9a7576679b3254f2e7f
shakadak <nathanael.pineau@gmail.com>
2015-06-27 20:49:46 +0200

    starting ttf test

--- Draw.ml
+++ Draw.ml
@@ -1,3 +1,10 @@
 let background s c = Sdlvideo.fill_rect s (Sdlvideo.map_RGB s c)
 
-let button _ _ _ _ = ()
+let button s r l a =
+    let f = Sdlvideo.map_RGB s (if a then Sdlvideo.black else Sdlvideo.white)
+    in let b = Sdlvideo.map_RGB s (if not a then Sdlvideo.black else Sdlvideo.white)
+    in let outer = Sdlvideo.rect (r.Sdlvideo.r_x - 2) (r.Sdlvideo.r_y - 2) (r.Sdlvideo.r_w + 4) (r.Sdlvideo.r_h + 4)
+    in let inner = Sdlvideo.rect (r.Sdlvideo.r_x - 1) (r.Sdlvideo.r_y - 1) (r.Sdlvideo.r_w + 2) (r.Sdlvideo.r_h + 2)
+    in Sdlvideo.fill_rect ~rect:outer s f
+    ; Sdlvideo.fill_rect ~rect:inner s b
+    ; Sdlvideo.fill_rect ~rect:r s f
