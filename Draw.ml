38a5293eff756f5359a11673a32b47a4f4aad2fd
shakadak <nathanael.pineau@gmail.com>
2015-06-28 11:23:34 +0200

    draw button

--- Draw.ml
+++ Draw.ml
@@ -10,14 +10,30 @@ module Button =
         let inner x y = Sdlvideo.rect (x + 3) (y + 3) (w - 6) (h - 6)
         let text x y tw = Sdlvideo.rect (x + (w - tw) / 2) (y + 4) 0 0
 
-        let thunder font color = Sdlttf.render_text_solid font "THUNDER" color
+        let draw screen (x, y) label colors =
+            let (lw, _, _) = Sdlvideo.surface_dims label
+            in let foreground = Sdlvideo.map_RGB screen (fst colors)
+            in let background = Sdlvideo.map_RGB screen (snd colors)
+            in Sdlvideo.fill_rect ~rect:(outer x y) screen background
+            ; Sdlvideo.fill_rect ~rect:(middle x y) screen foreground
+            ; Sdlvideo.fill_rect ~rect:(inner x y) screen background
+            ; Sdlvideo.blit_surface ~src:label ~dst:screen ~dst_rect:(text x y lw) ()
+
+        let eat screen coordinate font colors =
+            let label = Sdlttf.render_text_solid font "EAT" (fst colors)
+            in draw screen coordinate label colors
+
+        let thunder screen coordinate font colors =
+            let label = Sdlttf.render_text_solid font "THUNDER" (fst colors)
+            in draw screen coordinate label colors
+
+        let bath screen coordinate font colors =
+            let label = Sdlttf.render_text_solid font "BATH" (fst colors)
+            in draw screen coordinate label colors
+
+        let kill screen coordinate font colors =
+            let label = Sdlttf.render_text_solid font "KILL" (fst colors)
+            in draw screen coordinate label colors
     end
 
 let background screen c = Sdlvideo.fill_rect screen (Sdlvideo.map_RGB screen c)
-
-let button screen (x, y) label (foreground, background) =
-    let (lw, _, _) = Sdlvideo.surface_dims label
-    in Sdlvideo.fill_rect ~rect:(Button.outer x y) screen foreground
-    ; Sdlvideo.fill_rect ~rect:(Button.middle x y) screen background
-    ; Sdlvideo.fill_rect ~rect:(Button.inner x y) screen foreground
-    ; Sdlvideo.blit_surface ~src:label ~dst:screen ~dst_rect:(Button.text x y lw) ()
--- Draw.mli
+++ Draw.mli
@@ -1,8 +1,14 @@
+module Button :
+    sig
+        val eat : Sdlvideo.surface -> (int * int) -> Sdlttf.font
+        -> (Sdlvideo.color * Sdlvideo.color) -> unit
+        val thunder : Sdlvideo.surface -> (int * int) -> Sdlttf.font
+        -> (Sdlvideo.color * Sdlvideo.color) -> unit
+        val bath : Sdlvideo.surface -> (int * int) -> Sdlttf.font
+        -> (Sdlvideo.color * Sdlvideo.color) -> unit
+        val kill : Sdlvideo.surface -> (int * int) -> Sdlttf.font
+        -> (Sdlvideo.color * Sdlvideo.color) -> unit
+    end
+
 val background : Sdlvideo.surface -> Sdlvideo.color -> unit
 (* Fill the entire surface with the color provided *)
-
-val button :
-    Sdlvideo.surface -> (int * int) -> Sdlvideo.surface ->
-        (int32 * int32) -> unit
-(* Draw a button of size rect, containing the string passed.
- * The last parameter determine wether the button is activated or not. *)
