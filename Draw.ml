b3b62878df72f039ed7d31a879ad1db86d9c223d
fdaudre- <felix.daudre.vignier@gmail.com>
2015-06-28 18:01:20 +0200

    +Draw.StatBar

--- Draw.ml
+++ Draw.ml
@@ -39,4 +39,69 @@ module Button =
             in draw screen coordinate label colors
     end
 
+module StatBar =
+    struct
+        type stat = Health | Energy | Hygiene | Happy
+
+        (*let font = Sdlttf.open_font "hacked/HACKED.ttf" 42*)
+
+        let draw screen font st value =
+            let e_const = exp 1. in
+            let (x, y, label) =
+                match st with
+                | Health    ->  (20, 100, "Health")
+                | Energy    ->  (20, 250, "Energy")
+                | Hygiene   ->  (20, 400, "Hygiene")
+                | Happy     ->  (20, 550, "Happy")
+            in
+            let border_color = Sdlvideo.map_RGB screen (0, 0, 0) in
+            let rec draw_bar i =
+                let hei =
+                    let x_func = ((float_of_int i) /. 100.) -. 2. in
+                    int_of_float ((exp x_func) *. 30.)
+                in
+                let linear_interpolate src aim factor =
+                    let s = float_of_int src in
+                    let a = float_of_int aim in
+                    int_of_float ((a -. s) *. factor +. s)
+                in
+                let exp_interpolate src aim factor =
+                    let s = float_of_int src in
+                    let a = float_of_int aim in
+                    let f = ((10. ** factor) -. 1.) /. (10. -. 1.) in
+                    int_of_float ((a -. s) *. f +. s)
+                in
+                let log_interpolate src aim factor =
+                    let s = float_of_int src in
+                    let a = float_of_int aim in
+                    let f = log10 (  (factor *. (10. -. 1.)) +. 1.  ) in
+                    int_of_float ((a -. s) *. f +. s)
+                in
+                let line = Sdlvideo.rect (x + i) (y - hei) 1 hei in
+                let factor = (float_of_int i) /. 200. in
+                let bar_color = Sdlvideo.map_RGB screen (
+                        (exp_interpolate 255 0 factor),
+                        (log_interpolate 0 255 factor),
+                        (exp_interpolate 0 100 factor))
+                in
+                if i < 200 then
+                    begin
+                        if i < value then
+                            Sdlvideo.fill_rect ~rect:line screen bar_color
+                        ;Sdlvideo.put_pixel screen (x + i) y border_color
+                        ;Sdlvideo.put_pixel screen (x + i) (y - hei - 1)
+                                                                    border_color
+                        ;draw_bar (i + 1)
+                    end
+            in
+            let small_border = Sdlvideo.rect (x - 1) (y - 5) 1 6 in
+            let big_border = Sdlvideo.rect (x + 200) (y - 30) 1 31 in
+            let lbl_video = Sdlttf.render_text_solid font label (0, 0, 0) in
+            draw_bar 0
+            ;Sdlvideo.blit_surface ~src:lbl_video ~dst:screen
+                    ~dst_rect:(Sdlvideo.rect (x - 10) (y - 50) 0 0) ()
+            ;Sdlvideo.fill_rect ~rect:small_border screen border_color
+            ;Sdlvideo.fill_rect ~rect:big_border screen border_color
+    end
+
 let background screen c = Sdlvideo.fill_rect screen (Sdlvideo.map_RGB screen c)
