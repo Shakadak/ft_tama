ada5975a1d50d59c40c3791bcce9a19d5b89fa9d
shakadak <nathanael.pineau@gmail.com>
2015-06-28 16:46:59 +0200

    event advancing

--- Event.ml
+++ Event.ml
@@ -1,22 +1,31 @@
+type action = Press of Engine.action | Release of Engine.action | Quit
+
 let check_button (x, y) range =
     let r_x = range.Sdlvideo.r_x
     in let r_y = range.Sdlvideo.r_x
     in r_x <= x && x <= r_x + range.Sdlvideo.r_w
     && r_y <= y && y <= r_y + range.Sdlvideo.r_h
 
-let rec loop tama ui =
+let retrieve_button coord buttons =
+    List.fold_left
+    (fun acc (rect, action) ->
+        if check_button coord rect
+        then action
+        else acc) Engine.None buttons
+
+let get_action buttons =
     match Sdlevent.poll () with
     | Some(Sdlevent.QUIT)
     | Some(Sdlevent.KEYDOWN {Sdlevent.keysym=Sdlkey.KEY_ESCAPE}) ->
-            ()
+            Some(Quit)
     | Some(Sdlevent.MOUSEBUTTONUP
-    {Sdlevent.mbe_state=Sdlevent.RELEASED}) ->
-            ()
+    {Sdlevent.mbe_state=Sdlevent.RELEASED
+    ; Sdlevent.mbe_x=_ as x
+    ; Sdlevent.mbe_y=_ as y}) ->
+            Some(Release(retrieve_button (x, y) buttons))
     | Some(Sdlevent.MOUSEBUTTONDOWN
     {Sdlevent.mbe_state=Sdlevent.PRESSED
     ; Sdlevent.mbe_x=_ as x
     ; Sdlevent.mbe_y=_ as y}) ->
-            string_of_int x |> print_endline
-            ; string_of_int y |> print_endline
-            ; loop tama ui
-    | _ -> loop tama ui
+            Some(Press(retrieve_button (x, y) buttons))
+    | _ -> None
--- Event.mli
+++ Event.mli
@@ -1,3 +1,5 @@
+type action = Press of Engine.action | Release of Engine.action | Quit
+
 val check_button : (int * int) -> Sdlvideo.rect -> bool
 
-val loop : 'a -> 'b -> unit
+val get_action : (Sdlvideo.rect * Engine.action) list -> action option
